9922f31f9cd3f86ce20d77918da4fbba1a8852b4
uniqtor <39895137+uniqtor@users.noreply.github.com>
2018-07-30 11:52:02 -0400

    cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //Install express server
-const express = require('express');
+const express = require('express'), cors = require('cors');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/Uniqtor'));
+app.use(cors());
 
 app.get('/*', function(req,res) {
     
